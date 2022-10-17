6a1c676627a8cf7c1b924ca7e3045035eeb23528
Fatin Aisyah Binti Ghazali <aisyah.fatin@gmail.com>
2022-10-17 21:59:04 +0800

    Added dataset file path

--- fyp.py
+++ fyp.py
@@ -1,7 +1,9 @@
+import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data=pd.read_csv('https://docs.google.com/spreadsheets/d/1StLk2E1fqcHRUS-NxYM98UEFY0kdynUDXbamn_z3DyI/edit?usp=sharing', sep=';')
+st.header("Data Analytic Final Project")
+data=pd.read_csv(winequality-white.csv', sep=';')
 
 data.sample(5)
